7ded2e73e04328d10c30adb7c107d896c42c048f
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2020-02-11 21:45:41 +0100

    Add more tests to messages

--- src/test/java/com/javachess/server/message/MessageTests.java
+++ src/test/java/com/javachess/server/message/MessageTests.java
@@ -1,8 +1,5 @@
 package com.javachess.server.message;
 
-import com.javachess.logic.Game;
-import com.javachess.logic.Player;
-import com.javachess.server.GameOrchestrator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,12 +7,51 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MessageTests {
   @Test
-  @DisplayName("JoinGameIn should have an email field of type string")
-  public void JoinGameIn() {
+  @DisplayName("JoinGameIn should have an email field")
+  public void JoinGameInGetters() {
     JoinGameIn joinGameIn = new JoinGameIn("user@domain.tld");
     String expected = "user@domain.tld";
     String actual = joinGameIn.getEmail();
 
     assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("JoinGameIn should have an email setter")
+  public void JoinGameInSetter() {
+    JoinGameIn joinGameIn = new JoinGameIn("user@domain.tld");
+    String expected = "expected@domain.tld";
+
+    joinGameIn.setEmail(expected);
+    String actual = joinGameIn.getEmail();
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("LookingForGameOut should have message and gameId fields")
+  public void LookingForGameOutGetters() {
+    LookingForGameOut lookingForGameOut = new LookingForGameOut("message", 2);
+
+    String message = lookingForGameOut.getMessage();
+    int id = lookingForGameOut.getGameId();
+
+    assertEquals("message", message);
+    assertEquals(2, id);
+  }
+
+  @Test
+  @DisplayName("LookingForGameOut should have message and gameId setters")
+  public void LookingForGameOutSetters() {
+    LookingForGameOut lookingForGameOut = new LookingForGameOut("message", 2);
+
+    lookingForGameOut.setMessage("universe");
+    lookingForGameOut.setGameId(42);
+
+    String message = lookingForGameOut.getMessage();
+    int id = lookingForGameOut.getGameId();
+
+    assertEquals("universe", message);
+    assertEquals(42, id);
   }
 }
