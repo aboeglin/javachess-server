656c89ea3674f221e01a51ca56982d6c97062ee2
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2020-02-18 15:41:12 +0100

    Add tests for computeTrajectory in Bishop

--- src/test/java/com/javachess/logic/BishopTests.java
+++ src/test/java/com/javachess/logic/BishopTests.java
@@ -12,8 +12,8 @@ import java.util.List;
 class BishopTests {
 
   @Test
-  @DisplayName("computeTrajectory")
-  public void computeTrajectory() {
+  @DisplayName("computeTrajectory should also work for top right direction")
+  public void computeTrajectoryTopRight() {
     Position from = Position.of("b", "1");
     Position to = Position.of("h", "7");
     List<Position> expected = Arrays.asList(new Position[]{
@@ -29,16 +29,50 @@ class BishopTests {
   }
 
   @Test
-  @DisplayName("computeTrajectory should also work for decreasing numbers")
-  public void computeTrajectoryDec() {
-    Position from = Position.of("h", "7");
-    Position to = Position.of("b", "1");
+  @DisplayName("computeTrajectory should also work for top left direction")
+  public void computeTrajectoryTopLeft() {
+    Position from = Position.of("a", "7");
+    Position to = Position.of("g", "1");
     List<Position> expected = Arrays.asList(new Position[]{
-      Position.of("g", "6"),
-      Position.of("f", "5"),
+      Position.of("b", "6"),
+      Position.of("c", "5"),
+      Position.of("d", "4"),
+      Position.of("e", "3"),
+      Position.of("f", "2"),
+    });
+
+    List<Position> actual = Bishop.computeTrajectory(from, to);
+    assertArrayEquals(expected.toArray(), actual.toArray());
+  }
+
+  @Test
+  @DisplayName("computeTrajectory should also work for bottom left direction")
+  public void computeTrajectoryBottomLeft() {
+    Position from = Position.of("g", "7");
+    Position to = Position.of("a", "1");
+    List<Position> expected = Arrays.asList(new Position[]{
+      Position.of("f", "6"),
+      Position.of("e", "5"),
+      Position.of("d", "4"),
+      Position.of("c", "3"),
+      Position.of("b", "2"),
+    });
+
+    List<Position> actual = Bishop.computeTrajectory(from, to);
+    assertArrayEquals(expected.toArray(), actual.toArray());
+  }
+
+  @Test
+  @DisplayName("computeTrajectory should also work for bottom right direction")
+  public void computeTrajectoryBottomRight() {
+    Position from = Position.of("b", "7");
+    Position to = Position.of("h", "1");
+    List<Position> expected = Arrays.asList(new Position[]{
+      Position.of("c", "6"),
+      Position.of("d", "5"),
       Position.of("e", "4"),
-      Position.of("d", "3"),
-      Position.of("c", "2"),
+      Position.of("f", "3"),
+      Position.of("g", "2"),
     });
 
     List<Position> actual = Bishop.computeTrajectory(from, to);
