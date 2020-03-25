3da82fb5704e2ed57f9aa5ef676712717ddfe8f7
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2020-03-25 13:54:27 +0100

    Add PORT env var and resolve to 80 by default

--- src/main/java/com/javachess/server/Application.java
+++ src/main/java/com/javachess/server/Application.java
@@ -3,11 +3,19 @@ package com.javachess.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    String port = System.getenv("PORT");
+    if (port == null) {
+      port = "80";
+    }
+    SpringApplication app = new SpringApplication(Application.class);
+    app.setDefaultProperties(Collections.singletonMap("server.port", port));
+    app.run(args);
   }
 
 }
